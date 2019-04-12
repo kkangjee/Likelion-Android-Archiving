5a2ea60a362e05bb43ef00c85ea675e27fada8b8
kkangjee <wlgns337@gmail.com>
2019-04-12 17:37:12 +0900

    인스타그램 만들기 과제(하트 상태드러블 사용)

--- app/src/main/java/com/smu/sangmyung/likelionandroidarchiving/MainActivity.kt
+++ app/src/main/java/com/smu/sangmyung/likelionandroidarchiving/MainActivity.kt
@@ -2,11 +2,17 @@ package com.smu.sangmyung.likelionandroidarchiving
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //상단바 제거 메소드
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
     }
 }
